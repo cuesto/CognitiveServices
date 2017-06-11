fdcfd481a556ee02c3859ec6bb27b2ada40a1d11
Jonathan Cuesto <34cuesto@gmail.com>
2017-06-11 17:21:59 -0400

    Add project files.

---
+++ CognitiveServices.Service/Service.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace CognitiveServices
+{
+	public class Service
+	{
+		public static Service Instance { get; } = new Service();
+
+		public Service()
+		{
+			FaceServiceClient = new FaceServiceClient("a75804238fc9400bb3dcb2a95e3d4c06");
+			_personGroupId = Guid.NewGuid().ToString();
+		}
+
+        public List<Person> People { get; } = new List<Person>
+        {
+            new Person{
+                Name = "Mandela",
+                PhotoUrl = "http://www.photographydo.com/img/566834b7-5cd8-4664-adb5-691e95e43a78/ai/most-famous-portrait-photographers-annie-leibovitz.jpg",
+                City = "Guadalajara"
+            },
+            new Person{
+                Name = "Emma Watson",
+                PhotoUrl = "http://www2.pictures.zimbio.com/mp/quy5-Bi3lFwx.jpg",
+                City = "Leon"
+            },
+            new Person{
+                Name = "Angelina Jolie",
+                PhotoUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BODg3MzYwMjE4N15BMl5BanBnXkFtZTcwMjU5NzAzNw@@._V1_UY317_CR22,0,214,317_AL_.jpg",
+                 City = "Mexico"
+            }
+        };
+
+        string _personGroupId;
+
+		public FaceServiceClient FaceServiceClient { get; private set; }
+
+		public async Task RegisterEmployees()
+		{
+			await FaceServiceClient.CreatePersonGroupAsync(_personGroupId, "MVPs In Mexico");
+
+			foreach (var xmvp in People)
+			{
+				var p = await FaceServiceClient.CreatePersonAsync(_personGroupId, xmvp.Name);
+				await FaceServiceClient.AddPersonFaceAsync(_personGroupId, p.PersonId, xmvp.PhotoUrl);
+				xmvp.GroupId = _personGroupId;
+				xmvp.PersonId = p.PersonId.ToString();
+			}
+
+			await TrainPersonGroup();
+		}
+
+		public async Task<Person> FindSimilarFace(Stream faceData)
+		{
+			var faces = await FaceServiceClient.DetectAsync(faceData);
+			var faceIds = faces.Select(face => face.FaceId).ToArray();
+
+			var results = await FaceServiceClient.IdentifyAsync(_personGroupId, faceIds);
+			var result = results[0].Candidates[0].PersonId;
+
+			var person = await FaceServiceClient.GetPersonAsync(_personGroupId, result);
+			return new Person
+			{
+				Name = person.Name,
+				PersonId = result.ToString()
+			};
+		}
+
+		public async Task<bool> AddFace(Stream faceData, Person person)
+		{
+			try
+			{
+				var result = await FaceServiceClient.AddPersonFaceAsync(person.GroupId, Guid.Parse(person.PersonId), faceData);
+				if (result == null || string.IsNullOrWhiteSpace(result.PersistedFaceId.ToString()))
+					return false;
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public async Task TrainPersonGroup()
+		{
+			try
+			{
+				await FaceServiceClient.TrainPersonGroupAsync(_personGroupId);
+				TrainingStatus trainingStatus = null;
+				while (true)
+				{
+					trainingStatus = await FaceServiceClient.GetPersonGroupTrainingStatusAsync(_personGroupId);
+
+					if (trainingStatus.Status != Status.Running)
+					{
+						break;
+					}
+
+					await Task.Delay(1000);
+				}
+				return;
+			}
+			catch
+			{
+				return;
+			}
+		}
+
+		public async Task<Face> AnalyzeFace(Stream faceData)
+		{
+			var faces = await FaceServiceClient.DetectAsync(faceData, false, false, new List<FaceAttributeType> {
+				FaceAttributeType.Age,
+				FaceAttributeType.FacialHair,
+				FaceAttributeType.Gender,
+				FaceAttributeType.Glasses,
+				FaceAttributeType.HeadPose,
+				FaceAttributeType.Smile
+			});
+			if (faces.Length > 0)
+				return faces[0];
+			return null;
+		}
+	}
+}
